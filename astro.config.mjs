05f3d7fa90efee26bf9e66164d860906f8991a92
hartface <hartfacedev@gmail.com>
2026-06-12 20:07:14 +0300

    removed sitemap and updated config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,13 @@
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
-import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 export default defineConfig({
-	site: "https://hartface.github.io",
-	integrations: [
-		mdx(),
-		sitemap(),
-		react({
-			experimentalReactChildren: true,
-		}),
-		tailwind({
-			applyBaseStyles: false,
-		}),
-	],
+  site: "https://hartface.github.io",
+  integrations: [
+    mdx(),
+    react({ experimentalReactChildren: true }),
+    tailwind({ applyBaseStyles: false }),
+  ],
 });
